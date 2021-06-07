525b65be812b8b75184015c763caf4e81645a34d
davidaquilue <davidaquilue@github.com>
2021-06-07 15:49:23 +0200

    Flowchart

--- JR/recover.py
+++ JR/recover.py
@@ -7,15 +7,24 @@ from main import params
 from galgs import test_solution
 from networkJR import obtaindynamicsNET 
 from filefuns import check_create_results_folder, get_num
-from plotfuns import plot_inputs, plot_fftoutputs
+from plotfuns import plot_inputs, plot_fftoutputs, plotcouplings
 
 results_dir = check_create_results_folder()
 newfolder = get_num(results_dir)
 numnow = int(newfolder[-2:])-1
-newfolder = results_dir + '/' + str(numnow)
+if numnow < 10:
+    newfolder = results_dir + '/0' + str(numnow)
+else:
+    newfolder = results_dir + '/' + str(numnow)
 print(newfolder)
 
-solution = np.load(newfolder + '/best_ind.npy')
+#solution = np.load(newfolder + '/best_ind.npy')
+solution = np.ones(72)
+print(solution.shape)
+fig_couplings = plotcouplings(solution, params['matrix_exc'], params['matrix_inh'],
+                              (0, 1), params, True)
+fig_couplings.savefig(newfolder + "/bestweights.jpg")
+"""
 params['individual'] = solution
 
 params['signals'] = params['test_dataset'][0][0]
@@ -24,3 +33,4 @@ modidx = int(params['tspan'][-1]-10)*1000
 plot_inputs(y, params['signals'][:, -modidx:], params, t, newfolder)
 plot_fftoutputs(y, params, newfolder)
 test_solution(params, newfolder, whatplot='net')
+"""
